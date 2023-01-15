d8fa030bb8b29c41af0a674edbc847cb3974f2ee
amarchino <marchino.alessandro@gmail.com>
2023-01-15 21:49:42 +0000

    Lesson 10

--- my-course/Program.cs
+++ my-course/Program.cs
@@ -1,6 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace MyCourse
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = CreateWebHostBuilder(args).Build();
+            app.MapGet("/", () => "Hello World!");
+            app.Run();
 
-app.MapGet("/", () => "Hello World!");
+        }
 
-app.Run();
+        public static WebApplicationBuilder CreateWebHostBuilder(string[] args) =>
+            WebApplication.CreateBuilder(args);
+    }
+}
+
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
+
+// app.MapGet("/", () => "Hello World!");
+
+// app.Run();
