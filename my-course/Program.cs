4fdcb3a8d4f25dd336ba6221c0061206ded9057b
amarchino <marchino.alessandro@gmail.com>
2023-01-16 20:37:16 +0000

    Reset configuration to .NETCore 2.1

--- my-course/Program.cs
+++ my-course/Program.cs
@@ -1,23 +1,24 @@
-namespace MyCourse
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace my_course
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var app = CreateWebHostBuilder(args).Build();
-            app.MapGet("/", () => "Hello World!");
-            app.Run();
-
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static WebApplicationBuilder CreateWebHostBuilder(string[] args) =>
-            WebApplication.CreateBuilder(args);
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
-
-// var builder = WebApplication.CreateBuilder(args);
-// var app = builder.Build();
-
-// app.MapGet("/", () => "Hello World!");
-
-// app.Run();
