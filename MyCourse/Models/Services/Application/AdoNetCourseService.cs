f138269f6510657d3ec707cb18a0bf9c4abd469d
amarchino <marchino.alessandro@gmail.com>
2023-01-21 18:32:12 +0000

    Lesson 52

---
+++ MyCourse/Models/Services/Application/AdoNetCourseService.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using MyCourse.Models.Services.Infrastructure;
+using MyCourse.Models.ViewModels;
+
+namespace MyCourse.Models.Services.Application
+{
+    public class AdoNetCourseService : ICourseService
+    {
+        private readonly IDatabaseAccessor db;
+        public AdoNetCourseService(IDatabaseAccessor db)
+        {
+            this.db = db;
+        }
+        public CourseDetailViewModel GetCourse(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public List<CourseViewModel> GetCourses()
+        {
+            string query = "SELECT * FROM Courses";
+            DataSet dataSet = db.Query(query);
+            throw new NotImplementedException();
+        }
+    }
+}
