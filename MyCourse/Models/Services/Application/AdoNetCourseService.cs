f940111d62e218a79164cc2e5bbd1552c4dc9cea
amarchino <marchino.alessandro@gmail.com>
2023-01-21 20:13:29 +0000

    Lesson 55

--- MyCourse/Models/Services/Application/AdoNetCourseService.cs
+++ MyCourse/Models/Services/Application/AdoNetCourseService.cs
@@ -21,9 +21,16 @@ namespace MyCourse.Models.Services.Application
         }
         public List<CourseViewModel> GetCourses()
         {
-            string query = "SELECT * FROM Courses";
+            string query = "SELECT Id, Title, ImagePath, Author, Rating, FullPrice_Amount, FullPrice_Currency, CurrentPrice_Amount, CurrentPrice_Currency FROM Courses";
             DataSet dataSet = db.Query(query);
-            throw new NotImplementedException();
+            var dataTable = dataSet.Tables[0];
+            var courseList = new List<CourseViewModel>();
+            foreach(DataRow courseRow in dataTable.Rows)
+            {
+                var course = CourseViewModel.FromDataRow(courseRow);
+                courseList.Add(course);
+            }
+            return courseList;
         }
     }
 }
