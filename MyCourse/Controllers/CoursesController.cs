0135cd3f7015358f01b467829e2b7529aa8c1a24
amarchino <marchino.alessandro@gmail.com>
2023-01-21 16:46:23 +0000

    Lesson 047

--- MyCourse/Controllers/CoursesController.cs
+++ MyCourse/Controllers/CoursesController.cs
@@ -10,17 +10,20 @@ namespace MyCourse.Controllers
 {
   public class CoursesController : Controller
   {
+    private readonly CourseService courseService;
+    public CoursesController(CourseService courseService)
+    {
+      this.courseService = courseService;
+    }
     public IActionResult Index()
     {
       ViewData["Title"] = "Catalogo dei corsi";
-      var courseService = new CourseService();
       List<CourseViewModel> courses = courseService.GetCourses();
       return View(courses);
     }
 
     public IActionResult Detail(int id)
     {
-      var courseService = new CourseService();
       CourseDetailViewModel course = courseService.GetCourse(id);
       ViewData["Title"] = course.Title;
       return View(course);
