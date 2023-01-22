2de0851240f3eb728668cfb3f0801fca277a979d
amarchino <marchino.alessandro@gmail.com>
2023-01-22 13:52:35 +0000

    Lesson 65

--- MyCourse/Models/Services/Infrastructure/MyCourseDbContext.cs
+++ MyCourse/Models/Services/Infrastructure/MyCourseDbContext.cs
@@ -33,14 +33,34 @@ namespace MyCourse.Models.Services.Infrastructure
             modelBuilder.Entity<Course>(entity =>
             {
                 entity.ToTable("Courses");
-                entity.HasKey(course => course.Id);
+                entity.HasKey(course => course.Id); // Opzionale se il campo si chiama Id o CourseId
                 // entity.HasKey(course => new { course.Id, course.Author }); // Per chiavi primarie composite
+
+                entity.OwnsOne(course => course.CurrentPrice, builder => {
+                    // Opzionale se il nome delle colonne coincide
+                    builder
+                        .Property(money => money.Currency)
+                        .HasConversion<string>()
+                        .HasColumnName("CurrentPrice_Currency");
+                    builder.Property(money => money.Amount).HasColumnName("CurrentPrice_Amount");
+                });
+                entity.OwnsOne(course => course.FullPrice, builder => {
+                    builder.Property(money => money.Currency).HasConversion<string>();
+                });
+
+                entity.HasMany(course => course.Lessons)
+                    .WithOne(lesson => lesson.Course)
+                    .HasForeignKey(lesson => lesson.CourseId); // Opzionale se si chiama CourseId
             });
 
             modelBuilder.Entity<Lesson>(entity =>
             {
                 entity.ToTable("Lessons");
                 entity.HasKey(lesson => lesson.Id);
+
+                // entity.HasOne(lesson => lesson.Course)
+                //     .WithMany(course => course.Lessons)
+                //     .HasForeignKey(lesson => lesson.CourseId);
             });
         }
     }
