55c78da545839eaa495bcf6ccdcdd5f4070384e8
amarchino <marchino.alessandro@gmail.com>
2023-01-24 21:29:16 +0000

    Lesson 084

--- MyCourse/Startup.cs
+++ MyCourse/Startup.cs
@@ -29,6 +29,7 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddResponseCaching();
             services.AddMvc(options => {
                 CacheProfile homeProfile = new CacheProfile();
                 configuration.Bind("ResponseCache:Home", homeProfile);
@@ -63,6 +64,7 @@ namespace MyCourse
                 app.UseExceptionHandler("/Error");
             }
             app.UseStaticFiles();
+            app.UseResponseCaching();
             // app.UseMvcWithDefaultRoute();
             app.UseMvc(routeBuilder =>
             {
