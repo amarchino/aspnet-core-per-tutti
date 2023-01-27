56c8a116edea156b2a734249907c31afc79cab50
amarchino <marchino.alessandro@gmail.com>
2023-01-27 22:07:21 +0000

    Lesson 97

---
+++ MyCourse/Customizations/ViewComponents/PaginationBarViewComponent.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyCourse.Models.ViewModels;
+
+namespace MyCourse.Customizations.ViewComponents
+{
+    [ViewComponent(Name = "PaginationBar")]
+    public class PaginationBarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(CourseListViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
