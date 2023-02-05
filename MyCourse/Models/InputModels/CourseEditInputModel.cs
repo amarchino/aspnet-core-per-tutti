3e3e63e626799bd3c59c1d5f4f1db3a20a65fd91
amarchino <marchino.alessandro@gmail.com>
2023-02-05 22:13:28 +0000

    Lesson 124

--- MyCourse/Models/InputModels/CourseEditInputModel.cs
+++ MyCourse/Models/InputModels/CourseEditInputModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyCourse.Controllers;
 using MyCourse.Models.Enums;
@@ -36,6 +37,8 @@ namespace MyCourse.Models.InputModels
         [Required(ErrorMessage = "Il prezzo corrent è obbligatorio"),
         Display(Name = "Prezzo corrent")]
         public Money CurrentPrice { get; set; }
+        [Display(Name = "Nuova immagine...")]
+        public IFormFile Image { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
