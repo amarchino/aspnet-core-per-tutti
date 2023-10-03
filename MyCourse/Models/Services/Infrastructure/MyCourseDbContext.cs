637fed3a70ecd4184ff8caf12823e968da145fad
amarchino <marchino.alessandro@gmail.com>
2023-10-03 18:42:38 +0000

    Lesson 204

--- MyCourse/Models/Services/Infrastructure/MyCourseDbContext.cs
+++ MyCourse/Models/Services/Infrastructure/MyCourseDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using MyCourse.Models.Entities;
 using MyCourse.Models.Enums;
+using MyCourse.Models.ValueTypes;
 
 namespace MyCourse.Models.Services.Infrastructure;
 
@@ -16,31 +17,24 @@ public partial class MyCourseDbContext : IdentityDbContext<ApplicationUser>
     public virtual DbSet<Lesson>? Lessons { get; set; }
     public virtual DbSet<Subscription>? Subscriptions { get; set; }
 
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        // Pre-convention model configuration goes here
+        configurationBuilder.Properties<Currency>().HaveConversion<string>();
+        configurationBuilder.Properties<decimal>().HaveConversion<float>();
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Owned<Money>();
 
         modelBuilder.Entity<Course>(entity =>
         {
             entity.ToTable("Courses");
             entity.HasKey(course => course.Id); // Opzionale se il campo si chiama Id o CourseId
                                                 // entity.HasKey(course => new { course.Id, course.Author }); // Per chiavi primarie composite
-
-            entity.OwnsOne(course => course.CurrentPrice, builder =>
-            {
-                // Opzionale se il nome delle colonne coincide
-                builder
-                    .Property(money => money.Currency)
-                    .HasConversion<string>()
-                    .HasColumnName("CurrentPrice_Currency");
-                builder.Property(money => money.Amount)
-                    .HasConversion<float>()
-                    .HasColumnName("CurrentPrice_Amount");
-            });
-            entity.OwnsOne(course => course.FullPrice, builder =>
-            {
-                builder.Property(money => money.Currency).HasConversion<string>();
-            });
 
             entity.HasMany(course => course.Lessons)
                 .WithOne(lesson => lesson.Course)
@@ -57,16 +51,9 @@ public partial class MyCourseDbContext : IdentityDbContext<ApplicationUser>
             entity.HasMany(course => course.SubscribedUsers)
                 .WithMany(user => user.SubscribedCourses)
                 .UsingEntity<Subscription>(
-                    entity => entity.HasOne(subscription => subscription.User).WithMany().HasForeignKey(subscription => subscription.UserId),
-                    entity => entity.HasOne(subscription => subscription.Course).WithMany().HasForeignKey(subscription => subscription.CourseId),
                     entity =>
                     {
                         entity.ToTable("Subscriptions");
-                        entity.OwnsOne(subscription => subscription.Paid, builder =>
-                        {
-                            builder.Property(money => money.Currency).HasConversion<string>();
-                            builder.Property(money => money.Amount).HasConversion<float>();
-                        });
                     });
 
             entity.HasQueryFilter(course => course.Status != CourseStatus.Deleted);
